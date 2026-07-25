f827f3285918588bf99594b300ee97d2e6bdb248
Michael <micsamkid500@gmail.com>
2026-07-25 16:58:00 -0500

    adding deploy features to jenkinsfile

--- commands.sh
+++ commands.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
 export IMAGE_NAME=$1
-echo $PATH
-whoami
-hostname
-which docker-compose
-
 echo $PASS | docker login -u $USER --password-stdin
 docker-compose down -v
 docker-compose -f docker-compose.yaml up -d
